3b43a6bd660c4673b560c49f5ec0d5c74852a012
robmarcotte <marcotte.rt@gmail.com>
2020-12-11 18:48:06 -0500

    added DO_cleaning and automerge functions

---
+++ R/DO_cleaning_18to20.R
@@ -0,0 +1,86 @@
+# DO_cleaning_18to20
+#
+# Function to clean up noldus data with a data frame of known errors.
+DO_cleaning_18to20 = function(noldus_data, manual_fix){
+
+  # MOCA 18to20 Noldus template had a typo for WalkLoad. Replace WalkLoad periods with proper format
+  noldus_data$Behavior = str_replace(noldus_data$Behavior, pattern = 'WalkLoad ', replacement = 'WalkLoad (')
+
+  # Create a new data frame that will be used as a reference for cleaned noldus data
+  clean_behavior_activity = data.frame(Behavior = str_split(noldus_data$Behavior, ' \\(', simplify = T)[,1],
+                                       Modifier1 = noldus_data$METs,
+                                       Behavior_Compendium_MET = str_split(noldus_data$Behavior, ' \\(', simplify = T)[,2],
+                                       stringsAsFactors = F)
+
+  # Need to fix Modifier 2 and Activity Type Compendium METs separatly because of an error in the MOCA 18to20 template for these modifiers
+  Modifier2 = as.data.frame(noldus_data$Modifier_2)
+  Modifier2_first_rough = as.data.frame(str_split(Modifier2[,1], pattern = '\\(', simplify = T), stringsAsFactors = F)
+
+  # Fix column contents because they have mixed information (e.g. additional activity descriptor vs MET value)
+  if(ncol(Modifier2_first_rough) >2){
+    blank_index = which(Modifier2_first_rough[, ncol(Modifier2_first_rough)] == '')
+    if(length(blank_index)>0){
+      Modifier2_first_rough[blank_index, ncol(Modifier2_first_rough)] = Modifier2_first_rough[blank_index, (ncol(Modifier2_first_rough)-1)] # slide all MET values to last column
+      Modifier2_first_rough[blank_index, (ncol(Modifier2_first_rough)-1)] = '' # replace rows under V2 with blank in V3 with a blank
+      Modifier2_first = data.frame(Modifier2 = ifelse(Modifier2_first_rough[,(ncol(Modifier2_first_rough)-1)] == '',
+                                                      Modifier2_first_rough$V1,
+                                                      str_c(Modifier2_first_rough$V1, Modifier2_first_rough$V2, sep = '(')))
+    }
+  } else {
+    Modifier2_first = data.frame(Modifier2 = Modifier2_first_rough[,1])
+  }
+
+  # Create fixed Activity Type Compendium MET variable
+  blank_index = which(Modifier2[,ncol(Modifier2)] == '')
+
+  if(length(blank_index)>0){
+    # Fix the blank pieces
+    Modifier2[blank_index, ncol(Modifier2)] = Modifier2[blank_index, (ncol(Modifier2)-1)]
+    Modifier2[blank_index, (ncol(Modifier2)-1)] = ''
+
+    # Fix the multi-MET activities
+    Modifier2[-blank_index, ncol(Modifier2)] = str_c(Modifier2[-blank_index, (ncol(Modifier2)-1)], Modifier2[-blank_index, ncol(Modifier2)], sep = '.')
+    Modifier2[-blank_index, (ncol(Modifier2)-1)] = ''
+  }
+
+  if(ncol(Modifier2_first_rough) >2){
+    act_type_METs = str_replace(Modifier2_first_rough[, ncol(Modifier2_first_rough)], pattern = '\\)', '')
+  } else {
+    act_type_METs = str_replace(Modifier2_first_rough[, ncol(Modifier2_first_rough)], pattern = '\\)', '')
+    # act_type_METs = str_c(Modifier2_first_rough[,ncol(Modifier2_first_rough)], Modifier2[,ncol(Modifier2)], sep = '.')
+  }
+
+  # Remove the act_type_METs that have a '.' and replace with empty cell
+  point_periods = which(act_type_METs == '.')
+
+  if(length(point_periods) >0){
+    act_type_METs[point_periods] = ''
+  }
+
+  # Combine the fixed Modifier2 and Activity type METs to main clean_behav_act
+  clean_behavior_activity$Modifier2 = Modifier2_first$Modifier2
+  clean_behavior_activity$Activity_Compendium_MET = act_type_METs
+
+  clean_behavior_activity = clean_behavior_activity %>%
+    dplyr::select(Behavior, Modifier2, Modifier1, Behavior_Compendium_MET, Activity_Compendium_MET)
+
+  # Clear the extra white space at the end of activity types
+  clean_behavior_activity$Modifier2 = str_trim(clean_behavior_activity$Modifier2, side = 'right')
+
+  noldus_data$Behavior = clean_behavior_activity$Behavior
+  noldus_data$METs = clean_behavior_activity$Modifier1
+  noldus_data$Modifier_2 = clean_behavior_activity$Modifier2
+
+  noldus_data = left_join(noldus_data, manual_fix)
+  noldus_data$Modifier_2 = ifelse(noldus_data$Modifier_2 == 'Quiet', noldus_data$Behavior, noldus_data$Modifier_2)
+
+  noldus_data$Omit_me = 0
+
+  if(is.na(any(noldus_data$Reason != 0))| any(noldus_data$Reason != 0) == T){
+    noldus_data$METs = ifelse(str_detect(noldus_data$Reason, 'MET') == T,
+                              ifelse(!is.na(noldus_data$MET_Fix), noldus_data$MET_Fix, noldus_data$METs), noldus_data$METs)
+    noldus_data$Omit_me = ifelse(str_detect(noldus_data$Reason, 'Behav/Act')== T, 1, 0)
+  }
+
+  return(noldus_data)
+}
--- R/automerge_ag_do_data.R
+++ R/automerge_ag_do_data.R
@@ -1,14 +1,15 @@
 # automerge_ag_do_data
 #
 # Function to automate the process of merging ActiGraph and Noldus direct observation data
+# Note: do_screen as 'sequential' can only be completed if doparallel is FALSE. Otherwise, it has to be done as batch
 #
 # Library dependencies: foreach, doParallel, stringr, lubridate, dplyr, tidyr
 
-automerge_ag_do_data = function(ag_filepaths = NA, do_filepaths = NA, timestamps = NA,
+automerge_ag_do_data = function(ag_filepaths = NA, do_filepaths = NA, timestamps = NA, unique_indicator = NA,
                                 output_filepath = NA, visual_plots = TRUE,
-                                do_screen = c('sequential','batch'), doparallel = TRUE, cores = NA){
+                                do_screen = c('none','sequential','batch'), doparallel = TRUE, cores = NA){
 
-  if(doparallel == T){
+  if(doparallel == TRUE){
 
     cores = ifelse(is.na(cores), detectCores()/2, cores)
 
@@ -20,6 +21,56 @@ automerge_ag_do_data = function(ag_filepaths = NA, do_filepaths = NA, timestamps
     cores = 1
   }
 
-  # insert automation of merging, running the parallel loop on the number of direct observation files
+  cl = makeCluster(cores)
+
+  registerDoParallel(cl)
+
+  if(doparallel == T & do_screen == 'batch'){
+    do_data = foreach(iii = 1:length(do_filepaths), .packages = c('MOCAfunctions','tidyr','stringr','lubridate','dplyr'),
+                      .combine = 'rbind') %dopar% {
+                        noldus_data = DO_descriptives(do_filepaths[iii])
+                      }
+
+    do_data = DO_cleaning_18to20(do_data, manual_fix)
+
+    # Find unique coding combinations
+    do_unique = do_data %>% group_by(Behavior, Modifier2, Modifier1) %>% dplyr::summarize(n = n())
+
+  }
+
+  foreach(iii = 1:length(unique_indicator), .packages = c('tidyr','stringr','lubridate','dplyr')) %dopar% {
+    ag_index = str_which(ag_filepaths, unique_indicator[iii])
+    do_index = str_which(do_filepaths, unique_indicator[iii])
+    time_index = str_which(timestamps$participant, unique_indicator[iii])
+
+    session_start_time = timestamps$start[time_index]
+    session_date = ymd(timestamps$date[time_index])
+
+    # Read in and timestamp DO data
+    for(jjj in 1:length(do_index)){
+      noldus_data = DO_descriptives(do_filepaths[do_index[jjj]])
+      noldus_data = DO_msec_transform(noldus_data) # expand DO data to hundredths of seconds
+      noldus_data = DO_timestamp(noldus_data, session_start_time)
+      noldus_data$index = rep(seq(1, ceiling(nrow(noldus_data)/100)), each = 100)[1:nrow(noldus_data)]
+
+      noldus_data = data.frame(Participant = unique_indicator[iii],
+                               Date = session_date,
+                               Time = noldus_data$Time[seq(1, (nrow(noldus_data))+1, by = 100)][1:length(unique(noldus_data$index))],
+                               Behavior = tapply(noldus_data$Behavior, INDEX = noldus_data$index, FUN = majority_string),
+                               METs = tapply(noldus_data$METs, INDEX = noldus_data$index, FUN = majority_string),
+                               Modifier_2 = tapply(noldus_data$Modifier_2, INDEX = noldus_data$index, FUN = majority_string),
+                               Modifier_3 = tapply(noldus_data$Modifier_3, INDEX = noldus_data$index, FUN = majority_string),
+                               MET.level = tapply(noldus_data$MET.level, INDEX = noldus_data$index, FUN = majority_string))
+
+      noldus_data$Time = strip_time_from_fulldate(noldus_data$Time)
+
+      # Read in ActiGraph data, append DO data, export
+      for(aaa in 1:length(ag_index)){
+        ag_data = read_ag(ag_filepaths[ag_index[aaa]])
+
+
+      }
+    }
+  }
 
 }
