9314f920ff3fdad2ba10f18eb5641dca598d52de
robmarcotte <marcotte.rt@gmail.com>
2020-12-16 10:46:55 -0500

    debugged timestamp and do_index sourcing

--- R/AG_DO_merge.R
+++ R/AG_DO_merge.R
@@ -39,35 +39,36 @@ AG_DO_merge = function(ag_filepaths, do_filepaths, timestamps, do_time_indicator
   registerDoParallel(cl)
 
   # Parallel operations for a later date - RM 12/15/2020
-  foreach(iii = 1:length(do_time_indicator), .packages = c('tidyr','stringr','lubridate','dplyr', 'readxl', 'doParallel','foreach','data.table')) %dopar% {
+  foreach(iii = 1:length(ag_do_indicator), .packages = c('tidyr','stringr','lubridate','dplyr', 'readxl', 'doParallel','foreach','data.table')) %dopar% {
 
   # for(iii in 1:length(do_time_indicator)){
     ag_index = str_which(ag_filepaths, ag_do_indicator[iii])
     do_index = str_which(do_filepaths, ag_do_indicator[iii])
-    time_index = str_which(timestamps$participant, do_time_indicator[iii])
-
-    session_start_time = timestamps$start[time_index]
-    session_date = ymd(timestamps$date[time_index])
 
     # Read in and timestamp DO data
     for(jjj in 1:length(do_index)){
-      noldus_data = DO_descriptives(do_filepaths[do_index[jjj]])
-      noldus_data = DO_msec_transform(noldus_data) # expand DO data to hundredths of seconds
-      noldus_data = DO_timestamp(noldus_data, session_start_time)
+      time_index = str_which(timestamps$participant, do_time_indicator[do_index[jjj]])
+
+      session_start_time = timestamps$start[time_index]
+      session_date = ymd(timestamps$date[time_index])
+
+      noldus_data = MOCAfunctions::DO_descriptives(do_filepaths[do_index[jjj]])
+      noldus_data = MOCAfunctions::DO_msec_transform(noldus_data) # expand DO data to hundredths of seconds
+      noldus_data = MOCAfunctions::DO_timestamp(noldus_data, session_start_time)
       noldus_data$index = rep(seq(1, ceiling(nrow(noldus_data)/100)), each = 100)[1:nrow(noldus_data)]
 
       noldus_data = data.frame(Participant = do_time_indicator[iii],
                                Date = session_date,
                                Time = noldus_data$Time[seq(1, (nrow(noldus_data))+1, by = 100)][1:length(unique(noldus_data$index))],
-                               Behavior = tapply(noldus_data$Behavior, INDEX = noldus_data$index, FUN = majority_string),
-                               METs = tapply(noldus_data$METs, INDEX = noldus_data$index, FUN = majority_string),
-                               Modifier_2 = tapply(noldus_data$Modifier_2, INDEX = noldus_data$index, FUN = majority_string),
-                               Modifier_3 = tapply(noldus_data$Modifier_3, INDEX = noldus_data$index, FUN = majority_string),
-                               MET.level = tapply(noldus_data$MET.level, INDEX = noldus_data$index, FUN = majority_string))
+                               Behavior = tapply(noldus_data$Behavior, INDEX = noldus_data$index, FUN =  MOCAfunctions::majority_string),
+                               METs = tapply(noldus_data$METs, INDEX = noldus_data$index, FUN =  MOCAfunctions::majority_string),
+                               Modifier_2 = tapply(noldus_data$Modifier_2, INDEX = noldus_data$index, FUN =  MOCAfunctions::majority_string),
+                               Modifier_3 = tapply(noldus_data$Modifier_3, INDEX = noldus_data$index, FUN =  MOCAfunctions::majority_string),
+                               MET.level = tapply(noldus_data$MET.level, INDEX = noldus_data$index, FUN =  MOCAfunctions::majority_string))
 
-      noldus_data = DO_cleaning_18to20(noldus_data, do_fix)
+      noldus_data =  MOCAfunctions::DO_cleaning_18to20(noldus_data, do_fix)
 
-      noldus_data$Time = strip_time_from_fulldate(noldus_data$Time)
+      noldus_data$Time =  MOCAfunctions::strip_time_from_fulldate(noldus_data$Time)
 
       noldus_start = ymd_hms(str_c(noldus_data$Date[1], noldus_data$Time[1]))
       noldus_end_raw = ymd_hms(str_c(noldus_data$Date[1], noldus_data$Time[nrow(noldus_data)])) + seconds(1)
