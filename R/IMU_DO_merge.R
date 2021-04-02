caae876534d186bd8248438e9cd9b10a26455168
robmarcotte <marcotte.rt@gmail.com>
2021-04-02 00:10:44 -0400

    Fixed premature cluster termination for parallel computing

--- R/IMU_DO_merge.R
+++ R/IMU_DO_merge.R
@@ -100,7 +100,8 @@ IMU_DO_merge = function(imu_filepaths, do_filepaths, timestamps, do_time_indicat
       }
 
       # Parallel operations for a later date - RM 12/15/2020
-      stopCluster(cl)
     }
+    stopCluster(cl)
+
   }
 }
