bf699cd7daa0fea6524bd9f11311100d82288fdb
rmarcotte <rmarcotte@umass.edu>
2021-07-16 15:04:27 -0400

    Updated ag_epochr to require only Timestamp rather than both Date and Time stamps

--- R/ag_epochr.R
+++ R/ag_epochr.R
@@ -11,12 +11,11 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
   ag_data_1sec$index = rep(seq(1, new_rows, by = 1), each = epoch)[1:rows]
 
-  first_only_colnames = which(str_detect(colnames(ag_data_1sec), paste('file','stamp','Date','Time', sep = '|')))
+  first_only_colnames = which(str_detect(colnames(ag_data_1sec), paste('file','stamp', sep = '|')))
 
   epoch_data = ag_data_1sec[seq(1, nrow(ag_data_1sec), by = epoch), ..first_only_colnames]
 
-  count_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                     Time = dplyr::first(Time),
+  count_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                      Axis1 = sum(Axis1, na.rm = T),
                                                                      Axis2 = sum(Axis2, na.rm = T),
                                                                      Axis3 = sum(Axis3, na.rm = T)) %>% select(-index) %>%
@@ -26,8 +25,7 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
   # If there's step data,  reaggregate to epoch level
   if(any(str_detect(colnames(ag_data_1sec), 'Step'))){
-    step_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                       Time = dplyr::first(Time),
+    step_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                        Steps = sum(Steps, na.rm = T)) %>% select(-index)
 
     epoch_data = left_join(epoch_data, step_data)
@@ -35,8 +33,7 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
   # If there's inclinometer data, reaggregate to epoch level
   if(any(str_detect(colnames(ag_data_1sec), 'Inclinometer'))){
-    inclinometer_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                      Time = dplyr::first(Time),
+    inclinometer_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                       `Inclinometer Off` = sum(`Inclinometer Off`, na.rm = T),
                                                                       `Inclinometer Standing` = sum(`Inclinometer Standing`, na.rm = T),
                                                                       `Inclinometer Sitting` = sum(`Inclinometer Sitting`, na.rm = T),
@@ -48,8 +45,7 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
   # If there's lux data, reaggregate to epoch level
   if(any(str_detect(colnames(ag_data_1sec), 'Lux'))){
-    lux_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                              Time = dplyr::first(Time),
+    lux_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                               Lux = mean(Lux, na.rm =T)) %>%
       select(-index)
 
@@ -58,8 +54,7 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
   # if theres LFE data, repeat all steps for count axes and step data
   if(any(str_detect(colnames(ag_data_1sec), 'LFE'))){
-    count_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                       Time = dplyr::first(Time),
+    count_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                        Axis1_LFE = sum(Axis1_LFE, na.rm = T),
                                                                        Axis2_LFE = sum(Axis2_LFE, na.rm = T),
                                                                        Axis3_LFE = sum(Axis3_LFE, na.rm = T)) %>% select(-index) %>%
@@ -69,8 +64,7 @@ ag_epochr = function(ag_data_1sec,epoch = 60){
 
     # If there's step data, also reaggregate to epoch level
     if(any(str_detect(colnames(ag_data_1sec), 'Step'))){
-      step_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Date = dplyr::first(Date),
-                                                                        Time = dplyr::first(Time),
+      step_data = ag_data_1sec %>% group_by(index) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                         Steps_LFE = sum(Steps_LFE, na.rm = T)) %>% select(-index)
 
       epoch_data = left_join(epoch_data, step_data)
