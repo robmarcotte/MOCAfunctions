475190c904933c958123e11ee8fc25feb59e6516
rmarcotte <rmarcotte@umass.edu>
2021-07-19 22:45:19 -0400

    Updated sedsphere and hildebrand functions

--- R/hildebrand2014.R
+++ R/hildebrand2014.R
@@ -19,20 +19,20 @@ hildebrand2014 = function(enmo_data, samp_freq = 80, epoch = 60, expand_1sec = F
 
   # Collapse into 1-second windows by taking the mean of ENMO
   enmo_data$secs <- rep(1:secs,each=samp_freq)[1:n]
-  enmo_data = enmo_data %>% group_by(secs) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
+  enmo_data.secs = enmo_data %>% group_by(secs) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                 enmo_1sec = mean(ENMO, na.rm = T))
 
   # Collapse into 1-minute windows by taking the mean of 1-second ENMO
-  enmo_data$mins <- rep(1:mins,each=epoch)[1:nrow(enmo_data)]
-  enmo_data = enmo_data %>% group_by(mins) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
+  enmo_data.secs$mins <- rep(1:mins,each=epoch)[1:nrow(enmo_data.secs)]
+  enmo_data.min = enmo_data.secs %>% group_by(mins) %>% dplyr::summarize(Timestamp = dplyr::first(Timestamp),
                                                                 enmo_1min = mean(enmo_1sec, na.rm = T))
 
   # Apply Hildebrand cutpoints
-  enmo_data$Intensity = cut(enmo_data$enmo_1min, breaks = c(-Inf, 44.8, 100.6, 428.8, Inf), labels = c('Sedentary','LPA','MPA','VPA'), include.lowest = F)
+  enmo_data.min$Intensity = cut(enmo_data.min$enmo_1min, breaks = c(-Inf, 44.8, 100.6, 428.8, Inf), labels = c('Sedentary','LPA','MPA','VPA'), include.lowest = F)
 
   if(expand_1sec == T){
-    Hildebrand2014_Intensity = data.frame(Timestamp = acc_data_raw$Timestamp[seq(1, n, by = samp_freq)],
-                                          Hildebrand2014_Intensity = factor(rep(enmo_data$Intensity, each = epoch), levels =c('Sedentary','LPA','MPA','VPA'), labels =c('Sedentary','LPA','MPA','VPA'))[1:floor(n/samp_freq)])
+    Hildebrand2014_Intensity = data.frame(Timestamp = enmo_data.secs$Timestamp[1:floor(n/samp_freq)],
+                                          Hildebrand2014_Intensity = factor(rep(enmo_data.min$Intensity, each = epoch), levels =c('Sedentary','LPA','MPA','VPA'), labels =c('Sedentary','LPA','MPA','VPA'))[1:floor(n/samp_freq)])
     return(Hildebrand2014_Intensity)
 
   } else {
