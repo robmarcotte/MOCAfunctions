0f9823c5e7f159ccac49f641c0ab722e1d5ea8ac
robmarcotte <marcotte.rt@gmail.com>
2021-09-01 19:34:35 -0400

    Added "n" variable

--- R/staudenmayer2015_rf_loco.R
+++ R/staudenmayer2015_rf_loco.R
@@ -11,6 +11,8 @@
 #' @example staudenmayer2015_rf_loco(acc_data_raw)
 
 staudenmayer2015_rf_loco = function(acc_data_raw, mods_filepath = NA, samp_freq = 80, epoch = 15, expand_1sec = F){
+  n = nrow(acc_data_raw)
+
   acc_data_raw.sum = ag_feature_calc(acc_data_raw, window = epoch)
 
   # acc_data_raw$VMcorrG = abs(sqrt(acc_data_raw$AxisX^2 + acc_data_raw$AxisY^2 + acc_data_raw$AxisZ^2)-1)
