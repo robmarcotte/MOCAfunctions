eff3729d6ab497c32c56939fada8ec6bceabad28
rmarcotte <rmarcotte@umass.edu>
2022-09-18 17:27:29 -0500

    Updated to accomodate acceleration signal resampling if desired

--- R/read_ag.R
+++ R/read_ag.R
@@ -7,9 +7,10 @@
 #' @param device_serial_calibrate Binary indicator (T/F) of whether to use device-specific correction factor coefficients when there is insufficient non-movement periods in the data file for auto-calibration
 #' @param calibration_file Data frame containing device serial numbers and device-specific correction factor coefficients
 #' @param parse_timestamp Binary indicator (T/F) of whether the Timestamp should be separated into Date and Time columns. For larger files, this extends the read and return time.
+#' @param sf_coerce Binary indicator to determine if accelerometer signal should be resampled to expected sampling frequency (if different)
 #'
 #'
-read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, calibration_file, parse_timestamp = F){
+read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, calibration_file, parse_timestamp = F, samp_freq = 80, sf_coerce = F){
 
   check_data = fread(filepath,header = F,skip = 10, nrows = 1)[1,]
   if(is.numeric(check_data[1,])){
@@ -33,6 +34,8 @@ read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, ca
   date_time_start = mdy_hms(paste(date,start, sep = ' '))
 
   if(epoch < 1){
+    Timestamp = seq(from = date_time_start,to = (date_time_start + (file_length/frequency)), by = 1/frequency)[1:nrow(file_data)]
+
     if(any(colnames(file_data) == 'Timestamp')){
       file_data = file_data %>% dplyr::select(-Timestamp)
     }
@@ -42,11 +45,47 @@ read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, ca
                                      `Accelerometer Y` = as.numeric(`Accelerometer Y`),
                                      `Accelerometer Z` = as.numeric(`Accelerometer Z`))
 
+    # Possible that ACC signal actual vs expected samp_freqs don't match. If that's the case and user wants a single file, resample ACC signal to match expected samp_freq
+    # Note: Specific to ActiGraph files since sampling frequency is embedded in file header
+    sf_match = data.table::fread(filepath, nrow = 1)$V16 == samp_freq
+    if(sf_match == F & sf_coerce == T){
+      warning('Actual and expected Acceleration signal frequencies do not match. Resampling to expected SF....')
+
+      # Resample acceleration signals to proper sampling frequency
+      temp_data = data.frame(HEADER_TIME_STAMP = Timestamp,
+                             X = file_data$`Accelerometer X`,
+                             Y = file_data$`Accelerometer Y`,
+                             Z = file_data$`Accelerometer Z`)
+
+      # Resample signal using MIMSunit package
+      temp_data = MIMSunit::interpolate_signal(temp_data, sr = samp_freq)
+
+      # Resample signal using signal package... Incomplete
+      # for(col_index in 1:ncol(file_data)){
+      #   temp
+      #   # temp_signal = signal::interp(file_data[,..col_index], samp_freq/frequency, )
+      #
+      #   temp_data = bind_cols(temp_data, temp_signal)
+      # }
+
+
+      file_data = temp_data %>% dplyr::select(-HEADER_TIME_STAMP) %>%
+        dplyr::rename(`Accelerometer X` =  X,
+                      `Accelerometer Y` =  Y,
+                      `Accelerometer Z` =  Z)
+
+      Timestamp = seq(from = date_time_start,to = (date_time_start + (file_length/frequency)), by = 1/samp_freq)[1:nrow(file_data)]
+      frequency = samp_freq
+
+    }
+
+
     file_data = mutate(file_data,
                        VM = sqrt(`Accelerometer X`^2 + `Accelerometer Y`^2 + `Accelerometer Z`^2),
                        VMcorrG = abs(sqrt(`Accelerometer X`^2 + `Accelerometer Y`^2 + `Accelerometer Z`^2)-1))
 
     colnames(file_data) = c('AxisX','AxisY','AxisZ','VM','VMcorrG')
+
 
     if(ENMO_calibrate == T){
       if(nrow(file_data) > 12*60*60*frequency){
@@ -87,8 +126,6 @@ read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, ca
       colnames(file_data) = c('AxisX','AxisY','AxisZ', 'VM','VMcorrG','calX','calY','calZ','ENMO')
     }
 
-    Timestamp = seq(from = date_time_start,to = (date_time_start + (file_length/frequency)), by = 1/frequency)[1:nrow(file_data)]
-
   } else {
     # If Count data does not have column names when being read in
     if(is.numeric(check_data[1,])){
@@ -114,6 +151,7 @@ read_ag = function(filepath, ENMO_calibrate = T, device_serial_calibrate = T, ca
     colnames(new_data) = c('Filename','Timestamp','Date','Time',colnames(file_data))
 
   } else {
+
     file_data = dplyr::bind_cols(filename = basename(filepath),
                                  Timestamp = Timestamp,
                                  file_data)
